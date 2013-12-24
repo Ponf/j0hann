b7fc34821546de91df14c9a83c519411e4ffe973
Alexander Smirnov <alexandr.smirnov@corp.mail.ru>
2013-12-25 01:14:48 +0400

    jira task info. daemon

--- j0hann.rb
+++ j0hann.rb
@@ -1,22 +1,80 @@
 require 'cinch'
 require 'net/http/server'
+require 'jira'
 require 'cgi'
 require 'pp'
 
 bot = Cinch::Bot.new do
     configure do |c|
-        c.server = "smirn0v.ru"
-        c.user = "j0hann"
-        c.password = "bulletj0hann31337"
-        c.channels = ["#mailru-ios-mail"]
+        c.server = ""
+        c.user = ""
+        c.password = ""
+        c.channels = [""]
     end
 
-    on :message, "hello" do |m|
-        m.reply "Hello, #{m.user.nick}"
+    helpers do
+
+        def allowed(message)
+            #todo: runtime management ?
+            white_list = ['smirn0v','karimov','shmat1ay','panfilov','rumiantsev']
+            raise 'Not allowed' unless white_list.include?(message.user.authname)
+        end
+
+        def jira_client
+            options = {
+                :username => "",
+                :password => "",
+                :site     => 'https://jira.mail.ru/',
+                :context_path => '',
+                :auth_type => :basic
+            }
+
+            client = JIRA::Client.new(options)
+        end
+
+        def issue_uri_str(issue_id)
+            "https://jira.mail.ru/browse/#{issue_id}"
+        end
+        
+        def issue_info(issue_id, with_description = false)
+           client = jira_client() 
+           issue = client.Issue.find(issue_id)
+           result = nil
+           if issue 
+               fields = issue.attrs["fields"]
+               summary = nil
+               description = nil
+               if fields
+                   summary = fields["summary"]
+                   description = fields["description"] || "<no description>"
+               end 
+               if fields && summary
+                   result = "#{issue_uri_str(issue_id)}: #{summary}"
+                   if with_description
+                       result += "\n  #{description.lines[0..15].join("  ")}"
+                   end
+               end 
+           end
+           result
+        end
+
+        def ios_issue_info(short_issue_id,with_description=false)
+            return issue_info("iosmail-"+short_issue_id,with_description)
+        end
+    end
+
+    on :message, /(\!|(iosmail\-))([1-9][0-9]+)/ do |m,g1,g2,identifier|
+        allowed(m)
+        m.reply(ios_issue_info(identifier))
+    end
+
+    on :message, /^!task ([1-9][0-9]+)$/ do |m,query|
+        allowed(m)
+        m.reply(ios_issue_info(query,with_description=true))
     end
 end
 
-Net::HTTP::Server.run(:port => 8080, :background => true) do |request,stream|
+Net::HTTP::Server.run(:port => 8667, :background => true) do |request,stream|
   
   if request[:uri][:path] == '/post'
       params = CGI::parse(request[:uri][:query].str)
